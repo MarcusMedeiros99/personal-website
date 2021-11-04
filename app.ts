603d0beeacdda9c9301fedb0488307d739513cb9
MarcusMedeiros99 <marcuspara66@usp.br>
2021-11-04 19:39:40 -0400

    Fixing app.ts: listen called twice

--- app.ts
+++ app.ts
@@ -19,7 +19,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = "8000";
 }
-app.listen(port);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
